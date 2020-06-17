c87d41235659ea90edb417fa75155928a48f7467
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-06-17 20:11:23 +0300

    update the path

--- src/main/java/com/predoana/springboot/thymeleaf/controller/EmployeeController.java
+++ src/main/java/com/predoana/springboot/thymeleaf/controller/EmployeeController.java
@@ -31,7 +31,7 @@ public class EmployeeController {
 		//add to the string model
 		theModel.addAttribute("employees", theEmployees);
 		
-		return "list-employees";
+		return "/employees/list-employees";
 		
 	}
 }
