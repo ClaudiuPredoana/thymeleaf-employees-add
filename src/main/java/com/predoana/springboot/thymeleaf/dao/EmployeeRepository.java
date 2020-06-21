2ffb08e524874c4582ac74bbb871ef5a9e006877
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-06-21 12:17:42 +0300

    Add custom method to JPA Repository (sort by last name)

--- src/main/java/com/predoana/springboot/thymeleaf/dao/EmployeeRepository.java
+++ src/main/java/com/predoana/springboot/thymeleaf/dao/EmployeeRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.predoana.springboot.thymeleaf.entity.Employee;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+    //add method to sort by last name
+
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
