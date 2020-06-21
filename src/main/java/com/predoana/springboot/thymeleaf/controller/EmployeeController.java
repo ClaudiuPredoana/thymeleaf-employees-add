9ed1af93ca8b24bc088144c20d5abf0f5ebb23ab
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-06-21 12:12:33 +0300

    Process form data to save employee

--- src/main/java/com/predoana/springboot/thymeleaf/controller/EmployeeController.java
+++ src/main/java/com/predoana/springboot/thymeleaf/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.predoana.springboot.thymeleaf.entity.Employee;
@@ -44,5 +46,15 @@ public class EmployeeController {
 		theModel.addAttribute("employee", theEmployee);
 
 		return "employees/employee-form";
+	}
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+
+		//save the employee
+		employeeService.save(theEmployee);
+
+
+		//use a redirect to prevent duplicate submissions
+		return "redirect:/employees/list";
 	}
 }
